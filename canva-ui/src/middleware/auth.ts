bbe81060d219cf683e28a3f5325bb50685160b49
Kristian Tenfen <kristian.tenfen@cialdnb.com>
2024-12-17 14:46:41 -0300

    remove canva-ui

--- canva-ui/src/middleware/auth.ts
+++
@@ -1,6 +0,0 @@
-// middleware/auth.ts
-import { defineNuxtRouteMiddleware } from "#app";
-
-export default defineNuxtRouteMiddleware((to, from) => {
-  // Verificação de autenticação para acesso ao canvas
-});
